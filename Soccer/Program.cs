1e178d1de527955855e3aaa1abdd2293a8f6e2ef
Petr Zalizko <pzalizko@gmail.com>
2025-06-09 16:50:29 +0300

    Add OnModelCreating

--- Soccer/Program.cs
+++ Soccer/Program.cs
@@ -3,18 +3,15 @@ using Soccer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Получаем строку подключения из файла конфигурации
 string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// добавляем контекст ApplicationContext в качестве сервиса в приложение
-builder.Services.AddDbContext<SoccerContext>(options => options.UseSqlServer(connection));
+builder.Services.AddDbContext<SoccerContext>(options => options.UseSqlite(connection));
 
-// Добавляем сервисы MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-app.UseStaticFiles(); // обрабатывает запросы к файлам в папке wwwroot
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
